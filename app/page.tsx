a0ae266d2074a9eb95c1aff7a36a480c255b6db0
haripriyaro <haripriya@onetera.com>
2024-02-29 12:54:51 -0800

    added a minor landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/navigation/navbar";
+import PlatformLandingPage from "./platformLandingPage/platformLandingPage";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Navbar />
+      <PlatformLandingPage />
     </main>
   );
 }
