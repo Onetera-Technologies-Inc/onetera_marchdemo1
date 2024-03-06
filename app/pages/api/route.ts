206c91a010212fcf40d244c06f3a5f108f3eb36a
haripriyaro <haripriya@onetera.com>
2024-03-06 12:07:22 -0800

    added opneai key in file

--- app/pages/api/route.ts
+++ app/pages/api/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 
 export async function POST(req: Request, res: NextResponse) {
