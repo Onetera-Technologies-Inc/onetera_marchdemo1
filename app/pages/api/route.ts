81d6618900f271a2f6714b2107ef18ca8001a70c
haripriyaro <haripriya@onetera.com>
2024-03-06 12:24:19 -0800

    modified api key

--- app/pages/api/route.ts
+++ app/pages/api/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export async function POST(req: Request, res: NextResponse) {
